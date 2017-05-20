ced68cb63a682db87710ea7a9b82830cbf1a8f7d
Mahmoud Fathy <admiinx0@gmail.com>
2017-05-20 20:17:48 +0200

    delete unneeded interfaces

--- repo/src/main/java/com/admiinx/repo/RepoCache.java
+++ repo/src/main/java/com/admiinx/repo/RepoCache.java
@@ -1,5 +1,8 @@
 package com.admiinx.repo;
 
+import io.reactivex.Completable;
+import io.reactivex.Maybe;
+
 /**
  * A semi-persistent mapping from keys to values.
  * get cache values using {@link #get(Object)}
@@ -10,6 +13,40 @@ package com.admiinx.repo;
  * @param <Key>   the key type
  * @param <Value> the value type
  */
-public interface RepoCache<Key, Value> extends Readable<Key, Value>, Writable<Key, Value>, Cleanable<Key> {
+public interface RepoCache<Key, Value> {
 
+    /**
+     * Returns {@link Maybe} emit the value associated with {@code key}, or {@link Maybe#empty()} if there is no
+     * value for {@code key}.
+     *
+     * @param key uses to get the {@code Value}
+     * @return {@link Maybe} emit {@code Value} if Present otherwise complete
+     */
+    Maybe<Value> get(Key key);
+
+    /**
+     * Save and Associates {@code value} with {@code key}. If it's previously contained a
+     * value associated with {@code key}, the old value is replaced by {@code value}.
+     *
+     * @param key   the key which {@code value} associated with
+     * @param value the value which needed to put
+     * @return {@link Completable} that complete if put success
+     */
+    Completable put(Key key, Value value);
+
+
+    /**
+     * Discards the value associates with key {@code key}.
+     *
+     * @param key the key which {@code value} associated with
+     * @return {@link Completable} that complete if invalidate success
+     */
+    Completable invalidate(Key key);
+
+    /**
+     * Discards all entries.
+     *
+     * @return {@link Completable} that complete if invalidateAll success
+     */
+    Completable invalidateAll();
 }
